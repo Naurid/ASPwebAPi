17b3851d7b1e6de1990f0ec314c6ed67a4432d68
g.vilics <g.vilics@stag.technofuturtic.education>
2025-01-31 16:44:30 +0100

    yep

---
+++ Repository/Interfaces/ITaskRepository.cs
@@ -0,0 +1,10 @@
+﻿using Repository.Entities;
+
+namespace Repository.Interfaces;
+
+public interface ITaskRepository : IRepository<TaskEntity>
+{
+    public Task<TaskEntity> GetbyTitle<T>(string title);
+    public Task ChangeStatus(int id, string status);
+}
+
